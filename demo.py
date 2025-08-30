5629e2aa2822516fc7bfaa620ec5e19184081828
Digital Arjun <arjarya@gmail.com>
2025-08-30 12:49:58 +0530

    commit

--- demo.py
+++ demo.py
@@ -1,2 +1,7 @@
-user = input("You: ").loweer()
-if
+user = input("You: ").lower()
+if "hello" in user or "hi" in user:
+    print("AI: Hi there! 👋")
+elif "how are you" in user:
+    print("AI: I’m just code, but I’m great! 😃")
+else:
+    print("AI: Sorry, I don’t understand that yet.")
