51a9d29ab6485b1f40dcc259a67bd337182884cd
Digital Arjun <arjarya@gmail.com>
2025-09-03 01:31:00 +0530

    commit

---
+++ miniProjects/convertCase.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls')
+text = input("Enter text: ")
+print("Upper Case:", text.upper())
+print("Lower Case:", text.lower())
+print("Title Case:", text.title())
+print("Swap Case:", text.swapcase())
+print("Capitalize:", text.capitalize())
