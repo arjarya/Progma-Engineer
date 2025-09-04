1b7e060f726b7763d187cf553ddf0a4c51d4bd4c
Digital Arjun <arjarya@gmail.com>
2025-09-05 00:04:59 +0530

    commit

--- a i/chatbot.py
+++ a i/chatbot.py
@@ -13,7 +13,7 @@ response = {
     "what is your name": "i am your friendly AI chatbot.",
 }
 memory = {
-    "name": None
+   "name": None # None is python keyword for null value
 }
 while True:
     user = input("You:").lower()
@@ -21,11 +21,21 @@ while True:
      print("AI: Goodbye! have a great day")
      break 
     found = False
-    for key in response:
-       if key in user:  # check if any key is in user input:
-          print("AI:", response[key])
-          found = True
-          break 
+    if "my name is" in user:
+        memory["name"] = user.split("my name is")[-1].strip().capitalize()
+        print(f"AI: Nice to meet you, {memory['name']}")
+        found = True
+    elif "what is my name" in user:
+        if memory["name"]:
+           print(f"AI: your name is {memory['name']}")
+        else:
+           print("AI: I don't know your name yet")
+        found = True
+    else:
+         for key in response:
+             if key in user:  # check if any key is in user input:
+              print("AI:", response[key])
+              found = True
+              break 
     if not found:
        print("AI: I am sorry, i do not understand, Try again")
-
