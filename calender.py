0cf8342b1f310285037478589c57a2a10d5b2805
Digital Arjun <arjarya@gmail.com>
2025-09-05 14:19:33 +0530

    commit

--- calender.py
+++ calender.py
@@ -1,6 +1,12 @@
-import os, calendar
-os.system('cls')
-year = int(input("Enter year:"))
-month = int(input("Enter month(1-12)"))
-print(calendar.month(year, month))
-
+import os
+os.system("cls")
+import calendar
+year = 2025
+while True:
+ year = int(input("Enter year: "))
+ month = int(input("Enter month[1-12]: "))
+ if 1<= month <=12:
+    print(calendar.month(year, month))
+    break
+ else:
+    print("Invalid month")
