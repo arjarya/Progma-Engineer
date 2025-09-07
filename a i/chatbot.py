627176a9c6ac5b2872ed46d29a37ffbd75994123
Digital Arjun <arjarya@gmail.com>
2025-09-07 23:02:48 +0530

    commit

--- a i/chatbot.py
+++ a i/chatbot.py
@@ -20,10 +20,13 @@ while True:
      print("AI: Goodbye! have a great day")
      break 
     found = False
+    # Store name if user says "my name is ..."
     if "my name is" in user:
         memory["name"] = user.split("my name is")[-1].strip().capitalize()
         print(f"AI: Nice to meet you, {memory['name']}")
         found = True
+
+    # Recall name if user asks
     elif "what is my name" in user or "do you know my name" in user:
         if memory["name"]:
            print(f"AI: your name is {memory['name']}")
@@ -31,10 +34,13 @@ while True:
            print("AI: I don't know your name yet")
         found = True
     else:
+         user_words = user.split()
          for key in response:
-             if key in user:  # check if any key is in user input:
+             key_words = key.split()
+             if all(word in user_words for word in key_words): #use all()
               print("AI:", response[key])
               found = True
               break 
+    # Default reply
     if not found:
        print("AI: I am sorry, i do not understand, Try again")
