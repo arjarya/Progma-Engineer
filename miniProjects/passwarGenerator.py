9790d81f98d0c0b766355d3ab889631c70880072
Digital Arjun <arjarya@gmail.com>
2025-09-11 22:53:37 +0530

    commit

--- miniProjects/passwarGenerator.py
+++ miniProjects/passwarGenerator.py
@@ -1,8 +1,8 @@
 import random, string, os
 os.system('cls')
 
-print("Passwar Generator")
+print("Password Generator")
 user = int(input("You: Enter the length of passward: "))
 characters = string.ascii_letters + string.digits + string.punctuation
 passward = ''.join(random.choice(characters) for i in range(user))
-print("Your Passward is:", passward)
+print("Your Password is:", passward)
