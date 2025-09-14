7e8717e86d0ea72991ca4c98384c267f7bab7c25
Digital Arjun <arjarya@gmail.com>
2025-09-14 23:58:38 +0530

    commit

--- miniProjects/passwardGenerator.py
+++ miniProjects/passwardGenerator.py
@@ -1,16 +1,34 @@
-import string, random, os
-os.system('cls')
-print("password Generator")
-characters = string.ascii_letters + string.digits + string.punctuation
+import string, random, os; os.system('cls')
+print("Password Generator")
+
 
 while True:
-    length = int(input("Enter numbers only: "))
-    if 8 <= length <= 16:
-        password = ''.join(random.choice(characters) for i in range(length))
-        print("Your Password is: ", password)
-    else:
-        print("Password must be 8-16")
-    choice = input("Do you want another(y/n)? ").lower()
-    if choice != 'y':
-        print("Goodbye")
+    length = int(input("Enter Password Length: "))
+    if 8<= length <= 16:
         break
+    else:
+        print("Password length must be 8-16")
+
+
+include_upper = input("do you want uppercase letters? (y/n) ").lower() == "y"
+include_lower = input("do you want lowercase letter? (y/n) ").lower() == "y"
+include_digit = input("do you want digit? (y/n) ").lower() == "y"
+include_symbol = input("do you want symbol? (y/n)").lower() == "y"
+
+#Build character set
+characters = ""
+if include_upper:
+    characters += string.ascii_uppercase
+if include_lower:
+    characters += string.ascii_lowercase
+if include_digit:
+    characters += string.digits
+if include_symbol:
+    characters += string.punctuation
+
+if not characters:
+    print("default password: ")
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+password = ''.join(random.choice(characters) for i in range(length))
+print("Your Password is: ", password)
