3b759d251644e8bd31feee63f6dd68cd49d19afc
Digital Arjun <arjarya@gmail.com>
2025-09-16 23:40:19 +0530

    commit

--- calender.py
+++ calender.py
@@ -2,15 +2,15 @@ import os
 os.system("cls")
 import calendar
 year = int(input("Enter year(e.g, 2025): "))
+
 while True:
- month = int(input("Enter month[1-12]: "))
- if 1<= month <=12:
-    print(calendar.month(year, month))
-    
- else:
-    print("Invalid month")
-    continue # only skip back if month is invalid
- choice = input("Do you want see another month: (y/n)").lower().strip()
- if choice != "y":
-  print("goodbye")
-  break
+   month = int(input("Enter month[1-12]: "))
+   if 1<= month <=12:
+     print(calendar.month(year, month))
+   else:
+     print("Invalid month")
+     continue # only skip back if month is invalid
+   choice = input("Do you want see another month: (y/n)").lower().strip()
+   if choice != "y":
+      print("goodbye")
+      break
